591652233e0ed9bd13c215706004b3986d9787a6
shenzhef <martin.miauro@hotmail.com.ar>
2019-06-08 19:10:36 -0300

    segundo commit

--- platforms/browser/www/cordova_plugins.js
+++ platforms/browser/www/cordova_plugins.js
@@ -7,14 +7,23 @@ module.exports = [
         "clobbers": [
             "window.StatusBar"
         ]
+    },
+    {
+        "file": "plugins/es6-promise-plugin/www/promise.js",
+        "id": "es6-promise-plugin.Promise",
+        "pluginId": "es6-promise-plugin",
+        "runs": true
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.2.2",
     "cordova-plugin-console": "1.0.7",
-    "cordova-plugin-statusbar": "1.0.1"
+    "cordova-plugin-statusbar": "1.0.1",
+    "cordova-plugin-whitelist": "1.2.2",
+    "es6-promise-plugin": "4.2.2",
+    "cordova-plugin-compat": "1.2.0",
+    "phonegap-plugin-media-stream": "1.2.1"
 }
 // BOTTOM OF METADATA
 });
--- www/js/my-app.js
+++ www/js/my-app.js
@@ -1,29 +1,39 @@
 // Initialize app
-var myApp = new Framework7();
-
-
-// If we need to use custom DOM library, let's save it to $$ variable:
 var $$ = Dom7;
+var myApp = new Framework7({
+  root: '#app',
+language: 'es-ES',
+name: 'qr-scanner',
+theme: 'ios',
+id: 'fitness.capitan.qr-scanner'
+});
+
+
+var constraints = navigator.mediaDevices.getSupportedConstraints();
+console.log(constraints);
+navigator.mediaDevices.getUserMedia({
+    'video': {
+        facingMode: 'user'
+    }
+}).then(function(mediaStream) {
+  // do something with the media stream
+  var mediaControl = document.querySelector('video');
+  mediaControl.srcObject = mediaStream;
+  mediaControl.src = URL.createObjectURL(mediaStream)
+  console.log("funciona");
+});
 
 // Add view
-var mainView = myApp.addView('.view-main', {
-    // Because we want to use dynamic navbar, we need to enable it for this view:
-    dynamicNavbar: true
-});
+// var mainView = myApp.addView('.view-main', {
+//     // Because we want to use dynamic navbar, we need to enable it for this view:
+//     dynamicNavbar: true
+// });
 
 // Handle Cordova Device Ready Event
 $$(document).on('deviceready', function() {
     console.log("Device is ready!");
 });
 
-
-// Now we need to run the code that will be executed only for About page.
-
-// Option 1. Using page callback for page (for "about" page in this case) (recommended way):
-myApp.onPageInit('about', function (page) {
-    // Do something here for "about" page
-
-})
 
 // Option 2. Using one 'pageInit' event handler for all pages:
 $$(document).on('pageInit', function (e) {
